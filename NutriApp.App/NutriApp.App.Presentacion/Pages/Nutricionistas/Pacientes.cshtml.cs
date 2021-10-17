ed66e978bc79f7a10525934ad7bc960c5774c0c5
Carlos Soto <carlos950216@gmail.com>
2021-10-17 17:57:48 -0500

    estilos tablas

--- NutriApp.App/NutriApp.App.Presentacion/Pages/Nutricionistas/Pacientes.cshtml.cs
+++ NutriApp.App/NutriApp.App.Presentacion/Pages/Nutricionistas/Pacientes.cshtml.cs
@@ -4,13 +4,21 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using NutriApp.App.Persistencia;
+using NutriApp.App.Dominio;
 
 namespace NutriApp.App.Presentacion.Page.Nutricionistas.Pacientes
 {
     public class PacientesModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly IRepositoryNutricionista _repoNutricionista;
+        public PacientesModel(IRepositoryNutricionista _repoNutricionista){
+            this._repoNutricionista = _repoNutricionista;
+        }
+
+        public Nutricionista Nutricionista;
+        public void OnGet(int id){
+            Nutricionista = _repoNutricionista.GetNutricionista(id);
         }
     }
 }
